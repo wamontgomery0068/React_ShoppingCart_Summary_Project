d3a3955c72705328a33d852bc60f9f4bdfd15022
Your Name <wamontgomery0068@gmail.com>
2019-04-09 10:04:52 -0600

    Imported the SubTotal Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
+
+import SubTotal from './components/Subtotal/Subtotal';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 120
+    }
+  }
+
   render() {
     return (
       <div className = "Application_Container">
         <Container className = "Purchase_Card">
-          <h1> Hakuna Matata </h1>
+          <SubTotal price={this.state.total.toFixed(2)}/>
         </Container>
       </div>
     );
