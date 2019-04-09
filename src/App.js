7c30249a0ffb96f203dad59c33cc0add0122e9ee
Your Name <wamontgomery0068@gmail.com>
2019-04-09 12:10:54 -0600

    Imported the Pickup Savings Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+// Class Component
+
+// Imported Dependencies
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
 
+// Imported Components
 import SubTotal from './components/Subtotal/Subtotal';
+import PickupSavings from './components/PickupSavings/PickupSavings';
+
+// 
 import './App.css';
 
 class App extends Component {
@@ -9,8 +16,9 @@ class App extends Component {
     super(props);
 
     this.state = {
-      total: 120
-    }
+      total: 120,
+      pickupSavings: -3.85
+    } 
   }
 
   render() {
@@ -18,6 +26,7 @@ class App extends Component {
       <div className = "Application_Container">
         <Container className = "Purchase_Card">
           <SubTotal price={this.state.total.toFixed(2)}/>
+          <PickupSavings price={this.state.pickupSavings.toFixed(2)}/>
         </Container>
       </div>
     );
