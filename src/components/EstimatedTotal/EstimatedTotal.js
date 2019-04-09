425690f1f3273a1e00be37941893b560d542d9ff
Your Name <wamontgomery0068@gmail.com>
2019-04-09 13:18:06 -0600

    Built the EstimatedTotal file

---
+++ src/components/EstimatedTotal/EstimatedTotal.js
@@ -0,0 +1,15 @@
+// Class Component
+
+import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+export default class EstimatedTotal extends Component {
+    render() {
+        return (
+            <Row>
+                <Col md={6}> <h2> Est. Total </h2> </Col>
+                <Col md={6}> <h2> {`$${this.props.price}`} </h2> </Col>
+            </Row>
+        )
+    }
+}
