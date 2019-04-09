c9378ae2b93c8972457750b8b2f1f29802a776f0
Your Name <wamontgomery0068@gmail.com>
2019-04-09 09:33:28 -0600

    Imported Container from React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className = "App_Container">
-        <h1> Hakuna Matata </h1>
+      <div className = "Application_Container">
+        <Container className = "Purchase_Card">
+          <h1> Hakuna Matata </h1>
+        </Container>
       </div>
     );
   }
