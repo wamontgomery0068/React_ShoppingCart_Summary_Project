2d7145f2b228916e7c05d2e449e3d91482550b87
Your Name <wamontgomery0068@gmail.com>
2019-04-09 12:28:51 -0600

    Imported TaxesFees File

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 // Class Component
-
-// Imported Dependencies
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
 
-// Imported Components
 import SubTotal from './components/Subtotal/Subtotal';
 import PickupSavings from './components/PickupSavings/PickupSavings';
+import TaxesFees from './components/TaxesFees/TaxesFees';
 
-// 
 import './App.css';
 
 class App extends Component {
@@ -17,7 +14,8 @@ class App extends Component {
 
     this.state = {
       total: 120,
-      pickupSavings: -3.85
+      pickupSavings: -3.85,
+      taxes: 0
     } 
   }
 
@@ -27,6 +25,8 @@ class App extends Component {
         <Container className = "Purchase_Card">
           <SubTotal price={this.state.total.toFixed(2)}/>
           <PickupSavings price={this.state.pickupSavings.toFixed(2)}/>
+          <TaxesFees taxes={this.state.taxes.toFixed(2)} />
+          <hr />
         </Container>
       </div>
     );
