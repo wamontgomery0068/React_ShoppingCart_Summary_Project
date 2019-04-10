af95fac289028daf6a18dc2a56b32dc5393a5de0
Your Name <wamontgomery0068@gmail.com>
2019-04-10 06:53:57 -0600

    Imported the ItemDetails.js File

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
 
+// Imported Components
 import SubTotal from './components/Subtotal/Subtotal';
 import PickupSavings from './components/PickupSavings/PickupSavings';
 import TaxesFees from './components/TaxesFees/TaxesFees';
 import EstimatedTotal from './components/EstimatedTotal/EstimatedTotal';
+import ItemDetails from './components/ItemDetails/ItemDetails';
 
 import './App.css';
 
@@ -30,6 +32,7 @@ class App extends Component {
           <TaxesFees taxes={this.state.taxes.toFixed(2)} />
           <hr />
           <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)} />
+          <ItemDetails />
         </Container>
       </div>
     );
