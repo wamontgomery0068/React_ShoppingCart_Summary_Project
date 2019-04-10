4bb33eda8d2c31860f966bc0c35c83a2b1fdc5b3
Your Name <wamontgomery0068@gmail.com>
2019-04-10 09:37:25 -0600

    Changes Made

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Button, Collapse, Well, Media, Row, Col} from 'react-bootstrap';
+import { Button, Collapse, Media, Row, Col} from 'react-bootstrap';
+
+import './ItemDetails.css';
 
 export default class ItemDetails extends Component {
     constructor(props) {
@@ -25,7 +27,31 @@ export default class ItemDetails extends Component {
                 { this.state.open === false ? `+`: `-`}
                 </Button>
                 
+                <Collapse in={this.state.open}>
+                    <div className = "Collapse_Container">
+                        <img
+                            className = "Item_Image" 
+                            alt="jersey"
+                            src="https://fanatics.frgimages.com/FFImage/thumb.aspx?i=/productimages/_3380000/altimages/ff_3380166-c0d9d0ff8978abbfe394alt1_full.jpg&w=900"
+                        />
+                        <div className = "Item_Details">
+                            <p className = "Item_Name"> Ronald Acuña Jr Majestic Jersey </p>
+                            <p className = "Item_Description"> It features bold Atlanta Braves graphics that are perfect for showing your team pride as well as Cool Base technology that wicks away moisture. </p>
+                        </div>
+                    </div>
+                </Collapse>
+                
             </div>
         )
     }
 }
+
+{/* <Media>
+                            <img 
+                                width={150}
+                                height={150}
+                                alt="jersey"
+                                src="https://fanatics.frgimages.com/FFImage/thumb.aspx?i=/productimages/_3380000/altimages/ff_3380166-c0d9d0ff8978abbfe394alt1_full.jpg&w=900"
+                            />
+                        </Media>
+                        <h5> Robert Acuna Baseball Jersey </h5> */}
